bf2537de0156ce1bc48413baa61fad59552b2deb
asBrettisay <brett.caudill.is@gmail.com>
2016-02-11 22:44:45 -0700

    Retweeting and favoriting works correctly, and dynamically created elements have the correct behaviors. However, retweets on dynamically created objects do not update

--- js/animations.js
+++ js/animations.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
   var userName = '@billytweet';
   $.actionFavorite = $('.tweet-actions > ul :nth-child(3)');
   $.actionRetweet = $('.tweet-actions > ul :nth-child(2)');
-
   $.reTweet;
 
   function newTweet(tweet, reTweet) {
@@ -26,7 +25,7 @@ $(document).ready(function() {
     }
 
     $('#stream').prepend(function() {
-      return '<div class="tweet"> <div class="content"> <img class="avatar" src="img/alagoon.jpg" /> <strong class="fullname">Billy Tweet</strong> <span class="username">@billytweet</span> <p class="tweet-text">' + tweet + '</p> <div class="tweet-actions"> <ul> <li><span class="icon action-reply"></span> Reply</li> <li><span class="icon action-retweet"></span> Retweet</li> <li><span class="icon action-favorite"></span> Favorite</li> <li><span class="icon action-more"></span> More</li> </ul> </div> <div class="stats"> <div class="retweets"> <p class="num-retweets">0</p> <p>RETWEETS</p> </div> <div class="favorites"> <p class="num-favorites">0</p> <p>FAVORITES</p> </div> <div class="users-interact"> <div> <img src="img/alagoon.jpg" /> <img src="img/alagoon.jpg" /> </div> </div> <div class="time">' +  $.timeago(new Date()); + ' </div> </div> <div class="reply"> <img class="avatar" src="img/alagoon.jpg" /> <textarea class="tweet-compose" placeholder="Reply to @billytweet"/></textarea> </div> </div> </div>';
+      return '<div class="tweet"> <div class="content"> <img class="avatar" src="img/alagoon.jpg" /> <strong class="fullname">Billy Tweet</strong> <span class="username">@billytweet</span> <p class="tweet-text">' + tweet + '</p> <div class="tweet-actions"> <ul> <li><span class="icon action-reply"></span> Reply</li> <li><span class="icon action-retweet"></span> Retweet</li> <li><span class="icon action-favorite"></span> Favorite</li> <li><span class="icon action-more"></span> More</li> </ul> </div> <div class="stats"> <div class="retweets"> <p class="num-retweets">0</p> <p>RETWEETS</p> </div> <div class="favorites"> <p class="num-favorites">0</p> <p>FAVORITES</p> </div> <div class="users-interact"> <div> <img src="img/alagoon.jpg" /> </div> </div> <div class="time">' +  $.timeago(new Date()); + ' </div> </div> <div class="reply"> <img class="avatar" src="img/alagoon.jpg" /> <textarea class="tweet-compose" placeholder="Reply to @billytweet"/></textarea> </div> </div> </div>';
     });
   }
 
@@ -35,15 +34,24 @@ $(document).ready(function() {
     $('.reply').hide();
     $(this).find('.stats').show();
     $(this).find('.reply').show();
-  })
+  });
 
-  $('.content').mouseenter(function() {
-    $(this).find('.tweet-actions').show();
-  })
+  // $('.content').mouseenter(function() {
+  //   $(this).find('.tweet-actions').show();
+  // });
+  //
+  // $('.content').mouseleave(function() {
+  //   $('.tweet-actions').hide();
+  // })
 
-  $('.content').mouseleave(function() {
-    $('.tweet-actions').hide();
-  })
+  $('body').on({
+    mouseenter: function() {
+      $(this).find('.tweet-actions').show();
+    },
+    mouseleave: function() {
+      $('.tweet-actions').hide();
+    }
+  }, '.content');
 
   $('.tweet-compose').on('click', function() {
     $(this).height(100);
@@ -73,13 +81,28 @@ $(document).ready(function() {
     newTweet(tweet);
   });
 
-  actionFavorite.on('click', function() {
+  // actionFavorite.on('click', function() {
+  //   var target = $(this).parents('.content').find('.num-favorites');
+  //   var favorites = Number($(target).text());
+  //   $(target).text(favorites += 1);
+  // })
+
+  $('#stream').on('click', '.tweet-actions > ul :nth-child(3)', function() {
     var target = $(this).parents('.content').find('.num-favorites');
     var favorites = Number($(target).text());
     $(target).text(favorites += 1);
   })
 
-  actionRetweet.on('click', function () {
+  // actionRetweet.on('click', function () {
+  //   var target = $(this).parents('.content').find('.num-retweets');
+  //   var retweets = Number($(target).text());
+  //   var targetTweet = $(this).parents('.content').find('.tweet-text').text();
+  //   var targetUserName = $(this).parents('.content').find('.username').text();
+  //   $('#mainTweetBody').val('RT ' + targetUserName + ": " + targetTweet);
+  //   $.reTweet = {reTweet: true, target: target}
+  // })
+
+  $('#stream').on('click', '.tweet-actions > ul :nth-child(2)', function () {
     var target = $(this).parents('.content').find('.num-retweets');
     var retweets = Number($(target).text());
     var targetTweet = $(this).parents('.content').find('.tweet-text').text();
@@ -87,6 +110,7 @@ $(document).ready(function() {
     $('#mainTweetBody').val('RT ' + targetUserName + ": " + targetTweet);
     $.reTweet = {reTweet: true, target: target}
   })
+
 
 
 
